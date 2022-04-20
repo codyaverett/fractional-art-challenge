a079e8a4faf751f114d7e9478f867c8c4d76437a
Cody Averett <codyaverett@gmail.com>
2022-04-20 04:51:25 -0500

    api: refactor to a single sql server connection instance

--- practical/application/api/src/routes/hi.ts
+++ practical/application/api/src/routes/hi.ts
@@ -1,7 +1,8 @@
 import {
     FastifyInstance,
     FastifyPluginAsync,
-    FastifyPluginOptions
+    FastifyPluginOptions,
+    FastifyRequest
 } from 'fastify';
 import fp from 'fastify-plugin';
 
@@ -19,6 +20,18 @@ const hi: FastifyPluginAsync<MyPluginOptions> = async (
     options: FastifyPluginOptions
 ) => {
     try {
+        const sqlConnection = await new mssql().getConnection(defaultSqlConfig);
+        const sql = await sqlConnection.connect();
+
+        async function logApiUsage(request: FastifyRequest, event: string) {
+            // Logs on access. All the repetetive stuff should be moved to fastify middleware layer
+            // sql injection risk too.  Can mitigate with stored procedures (prefered) or cleaning any inputs
+            const apikey = request.headers.authorization;
+            const nowTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+            // const insertRequest = sqlConnection.request();
+            await sql.query(`INSERT INTO application.api.access_log VALUES('${apikey}','${nowTime}', '${event}')`);
+        }
+
         api.get('/hi', {}, async (request, reply) => {
             try {
                 return reply.code(200).send(["one", "two", "one", "two"]);
@@ -28,20 +41,12 @@ const hi: FastifyPluginAsync<MyPluginOptions> = async (
             }
         });
 
-        api.get('/get/access_keys', {}, async (request, reply) => {
+        const get_access_keys_route = '/get/access_keys';
+        api.get(get_access_keys_route, {}, async (request, reply) => {
             try {
                 await validateApiKey(request, async () => {
-                    const sqlConnection = await new mssql().getConnection(defaultSqlConfig);
-                    const sql = await sqlConnection.connect();
-
-                    // Logs on access. All the repetetive stuff should be moved to fastify middleware layer
-                    // sql injection risk too.  Can mitigate with stored procedures (prefered) or cleaning any inputs
-                    const key = request.headers.authorization;
-                    const nowTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-                    // const insertRequest = sqlConnection.request();
-                    await sql.query(`INSERT INTO application.api.access_log VALUES('${key}','${nowTime}', '/get/access_keys')`);
-
-                    const result = await sql.query("SELECT * FROM application.api.access_key")
+                    await logApiUsage(request, get_access_keys_route);
+                    const result = await sql.query("SELECT * FROM application.api.access_key");
 
                     return reply.code(200).send(result.recordset);
                 });
@@ -51,20 +56,11 @@ const hi: FastifyPluginAsync<MyPluginOptions> = async (
             }
         });
 
-        api.get('/get/access_logs', {}, async (request, reply) => {
+        const get_access_logs_route = '/get/access_logs';
+        api.get(get_access_logs_route, {}, async (request, reply) => {
             try {
                 await validateApiKey(request, async () => {
-
-                    const sqlConnection = await new mssql().getConnection(defaultSqlConfig);
-                    const sql = await sqlConnection.connect();
-
-                    // Logs on access. All the repetetive stuff should be moved to fastify middleware layer
-                    // sql injection risk too.  Can mitigate with stored procedures (prefered) or cleaning any inputs
-                    const key = request.headers.authorization;
-                    const nowTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
-                    // const insertRequest = sqlConnection.request();
-                    await sql.query(`INSERT INTO application.api.access_log VALUES('${key}','${nowTime}', '/get/access_logs')`);
-
+                    await logApiUsage(request, get_access_logs_route);
                     const result = await sql.query("SELECT * FROM application.api.access_log")
 
                     return reply.code(200).send(result.recordset);
@@ -76,13 +72,16 @@ const hi: FastifyPluginAsync<MyPluginOptions> = async (
         });
 
         // lookup mobile network operator provider
-        api.get('/get/mno/:mnoId', {}, async (request, reply) => {
+        const get_mno_byMnoId_route = '/get/mno';
+        api.get(`${get_mno_byMnoId_route}/:mnoId`, {}, async (request, reply) => {
             try {
-                const { mnoId } = request.params as { mnoId: string };
+                await validateApiKey(request, async () => {
+                    const { mnoId } = request.params as { mnoId: string };
+                    await logApiUsage(request, `${get_mno_byMnoId_route}/${mnoId}`);
+                    const lookup = lookupByMobileOperatorId(mnoId);
 
-                const lookup = lookupByMobileOperatorId(mnoId);
-
-                return reply.code(200).send(lookup || {});
+                    return reply.code(200).send(lookup || {});
+                });
             } catch (error) {
                 return reply.send(500);
             }
